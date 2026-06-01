803337d5595bacd9606600d5cca02a953433fb85
Sanika Thorat <sanikathorat451@gmail.com>
2026-06-01 14:50:03 +0530

    Add files via upload

---
+++ table 7.java
@@ -0,0 +1,8 @@
+class forloops {
+public static void main(String[] args) {
+for (int i = 1; i <= 10; i++) {
+System.out.println("7 x " + i + " = " + (7 * i));
+}
+System.out.println("Loop has ended.");
+}
+}
